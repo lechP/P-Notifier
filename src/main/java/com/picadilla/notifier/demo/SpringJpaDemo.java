97b654edc641df9b41b6994c14b7f7c6fa91adb0
LPI <cardano.p@gmail.com>
2016-03-11 07:58:08 +0100

    [LPI] introduced logging

--- src/main/java/com/picadilla/notifier/demo/SpringJpaDemo.java
+++ src/main/java/com/picadilla/notifier/demo/SpringJpaDemo.java
@@ -1,6 +1,8 @@
 package com.picadilla.notifier.demo;
 
 import com.picadilla.notifier.model.Notification;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,22 +11,24 @@ import java.util.List;
 
 public class SpringJpaDemo {
 
+    private static final Log log = LogFactory.getLog(SpringJpaDemo.class);
+
     public static void main(String[] args) {
-        System.out.print("****************** BEGINNING DEMO ****************");
+        log.info("The demo begins");
 
         ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring-config.xml");
         NotificationService service = (NotificationService) context.getBean("notificationService");
 
         Notification notification = Notification.newNotification("abcd@uvaw.com", new Date());
         service.saveNotification(notification);
-        System.out.println(notification);
+        log.info("Succesfully saved: " + notification);
 
         List<Notification> all = service.getAllNotifications();
         for (Notification notification1 : all) {
-            System.out.println(notification1);
+            log.warn("Found notification in db: " + notification1);
         }
 
-        System.out.println("******************* FINITO *******************");
+        log.info("good night");
     }
 
 }
